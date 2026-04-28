56640d5b60e241dac97b4e6036060969084a3d57
mf-mouse <qyv13@txstate.edu>
2026-04-28 14:14:55 -0500

    Update1 test2_send_out.R
    with annotations

--- R/test2_send_out.R
+++ R/test2_send_out.R
@@ -62,13 +62,13 @@ play_rodent_game <- function(col_2, col_3, col_4){ #arguments are cols
     } else {
     Search_Inat <- get_inat_obs(taxon_name = matches, photo_license = "any", maxresults = 100)#search rinat
     ImageCheck <- Search_Inat[Search_Inat$image_url != "", ]
-    img_url <- ImageCheck$image_url[2]#get an image url by index
+    img_url <- ImageCheck$image_url[2]#get an image url by index - add in option to ask if want more than one image, if so, ask multiple from inat
     picture_raw <- readJPEG(getURLContent(img_url), native = TRUE)#read in in the jpeg
-    res = dim(picture_raw)[2:1]#get the resolution [x,y]#
+    res = dim(picture_raw)[2:1]#get the resolution [x,y]
     plot(1,1,xlim=c(1,res[1]),ylim=c(1,res[2]),
          asp=1,type='n',xaxs='i',yaxs='i',xaxt='n',yaxt='n',xlab='',ylab='',bty='n')
     rasterImage(picture_raw,1,1,res[1],res[2])
-    }
+    } ###needs to fail gracefully if iNat API down
   }
 }
 
